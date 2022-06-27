26c6ce00592be53ef62f3f3c94eee810808bef79
Ramya <ramya.balapala143@gmail.com>
2022-06-27 05:16:52 +0000

    Result

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div className="card-homepage-container">
+    <h1 className="card-main-heading">Congratulations</h1>
+    <div className="card-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="image"
+      />
+      <h1 className="user-name">Kiran V</h1>
+      <p className="user-college">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
